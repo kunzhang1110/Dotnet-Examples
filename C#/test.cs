315948c313985bd61e8d1773fd0148c4bafc8c0d
Kun Zhang <kunzhang1110@gmail.com>
2024-07-27 07:02:41 +1000

    Delete test.cs

--- C#/test.cs
+++
@@ -1,9 +0,0 @@
-{
-    "MyOptions": {
-        "ConnectionStrings": {
-            "DefaultConnection": "Server=(localdb)\\mssqllocaldb;Database=MyDatabase;Trusted_Connection=True;"
-        },
-        "EnableFeatureA": true,
-        "MaxRetryAttempts": 3
-    }
-}
